4c0329f6dbca8467e2c0efaa2ed911e38dc387d9
willy jin <willy810110@gmail.com>
2023-05-08 17:42:28 +0800

    ver1.0

---
+++ products.py
@@ -0,0 +1,15 @@
+products = []
+
+while True:
+	name = input("請輸入商品名稱: ")
+	if name == 'q':
+		break
+	price = input('請輸入商品價格: ')
+	##-------------------
+	p = []
+	p.append(name)
+	p.append(price)
+	products.append(p)
+	#可簡寫為 products.append([nema, price])
+    ##--------------------
+print(products)
